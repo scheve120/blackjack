7fef69d7c39507181ae7a5f4bf81b102fa040728
David Berg <remotec@live.nl>
2020-03-15 12:07:09 +0100

    (WIP) Vraag over het verdelen
    Poging om de kaarten netjes in methods te stoppen om vervolgens de array later weer aan te roepen. Maar hoe?

--- Spel/src/Kaarten/Kaarten.java
+++ Spel/src/Kaarten/Kaarten.java
@@ -5,61 +5,75 @@ import java.util.Random;
 // ToDO Met de arrays oefenen.
 
 public class Kaarten {
-    /*
-    Deze instances maken de kaarten arrays aan.
-     */
-    ArrayList<Kaart> kaarten = new ArrayList<Kaart>();
-    ArrayList<Kaart> kaartType = new ArrayList<Kaart>();
-    ArrayList<Kaart> hogeKaarten = new ArrayList<Kaart>();
+
 
     /*
-    Kaarten set maakt de pak kaarten aan.
-     */
-    void KaartenSet() {
-        this.kaartType.add(new Kaart("Harten"));
+        Deze maakt de kaarten set.
+    */
+    void MaakKaartenSet() {
+    /*
+        Deze instances maken de kaarten arrays aan.
+    */
+        ArrayList<Kaart> kaarten = new ArrayList<Kaart>();
+        ArrayList<Kaart> kaartType = new ArrayList<Kaart>();
+        ArrayList<Kaart> hogeKaarten = new ArrayList<Kaart>();
+
+        kaartType.add(new Kaart("Harten"));
         kaartType.add(new Kaart("Schoppen"));
         kaartType.add(new Kaart("Klaver"));
         kaartType.add(new Kaart("Ruiten"));
 
         for (int i = 2; i <= 10; i++) {
-            this.kaarten.add(new Kaart(i));
+            kaarten.add(new Kaart(i));
         }
         kaarten.add(new Kaart('J'));
         kaarten.add(new Kaart('Q'));
         kaarten.add(new Kaart('K'));
         kaarten.add(new Kaart('Ä'));
+
     }
 
     /*
-    ToonKaartenSet Kan je de huidige kaarten deck zien of tonen.
-     */
+        ToonKaartenSet Kan je de huidige kaarten deck zien of tonen.
+    */
     public void ToonKaartenSet() {
-        for (Kaart nummer : this.kaarten) {
-            for (Kaart soort : this.kaartType) {
-                for (Kaart hogekaarten : this.hogeKaarten) {
-                    System.out.println(hogekaarten.hogekaarten);
-                    System.out.println("test of de kaart type 1 omhoog gaat. " + soort.type + nummer.hogekaarten);
-                }
+        
+        for (Kaart nummer : MaakKaartenSet) {
+            for (Kaart soort : MaakKaartenSet) {
+
+                System.out.println("test of de kaart type 1 omhoog gaat. " + soort.type + nummer.waarde);
+                System.out.println("test of de kaart type 1 omhoog gaat. " + soort.type + nummer.hogekaarten);
+
             }
         }
+
+        Random random = new Random();
+        int random1 = random.nextInt(13);
+        int random2 = random.nextInt(3);
+
+        if (random1 <= 8) {
+            System.out.println("Random1 lage kaarten == "+ kaarten.get(random1).waarde + " En random2 == " + kaartType.get(random2).type);
+        } else if (random1 >= 9) {
+            System.out.println("Random1 hogekaart == " + kaarten.get(random1).hogekaarten + " En random2 == " + kaartType.get(random2).type);
+        }
     }
+
     public static void main(String[] args) {
-        Kaarten toonkaartenset = new Kaarten();
-        toonkaartenset.ToonKaartenSet();
+        Kaarten toonkaarten = new Kaarten();
+        toonkaarten.MaakKaartenSet();
+
         /*
         Hier wordt er een random nummer gegeven die vervolgens een kaart pakt.
         Deze zal in de dealer/bank class worden gebruikt.
          */
-        Random random = new Random();
-        int random1 = random.nextInt(13);
-        int random2 = random.nextInt(3);
+
 
         /*
         Hier wordt er gekeken of de random lager of gelijk is aan index nummer 8 voor kaart nummer 10.
         Als deze hoger is dan worden de hoge kaarten: Boer, Vrouw, Koning, Ä getrokken.
         */
 
-        /*
+/*
         if (random1 <= 8) {
             System.out.println("Random1 lage kaarten == "+ kaarten.get(random1).waarde +
                                 " En random2 == " + kaartType.get(random2).type);
@@ -72,7 +86,7 @@ public class Kaarten {
             System.out.println(random1);
         }
 
-         */
+ */
     }
 }
 
@@ -94,12 +108,15 @@ class Kaart{
         this.type = type;
         this.waarde = waarde;
     }
+
     Kaart(String type) {
         this.type = type;
     }
+
     Kaart(int waarde) {
         this.waarde = waarde;
     }
+
     Kaart(char hogekaarten) {
         this.hogekaarten = hogekaarten;
     }
