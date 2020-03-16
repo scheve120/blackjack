b79af984c5f47764e0193a4ffda3714a3231889f
David Berg <remotec@live.nl>
2020-03-16 07:48:44 +0100

    (WIP)Kaarten deck is werkende
    De kaarten deck is verder opgeschoond en is nu werkende om verder te kunnen ontwikkelen.

--- Spel/src/Kaarten/Kaarten.java
+++ Spel/src/Kaarten/Kaarten.java
@@ -6,17 +6,15 @@ import java.util.Random;
 
 public class Kaarten {
 
-
     /*
         Deze maakt de kaarten set.
     */
-    void MaakKaartenSet() {
+    static void MaakKaartenSet(int randomtype, int randomwaarde, String toonalles) {
     /*
         Deze instances maken de kaarten arrays aan.
     */
         ArrayList<Kaart> kaarten = new ArrayList<Kaart>();
         ArrayList<Kaart> kaartType = new ArrayList<Kaart>();
-        ArrayList<Kaart> hogeKaarten = new ArrayList<Kaart>();
 
         kaartType.add(new Kaart("Harten"));
         kaartType.add(new Kaart("Schoppen"));
@@ -26,77 +24,36 @@ public class Kaarten {
         for (int i = 2; i <= 10; i++) {
             kaarten.add(new Kaart(i));
         }
+
         kaarten.add(new Kaart('J'));
         kaarten.add(new Kaart('Q'));
         kaarten.add(new Kaart('K'));
         kaarten.add(new Kaart('Ä'));
+
+        if (randomwaarde <= 8) {
+            System.out.println("Dit is uw kaart: "+ kaartType.get(randomtype).type + " " + kaarten.get(randomwaarde).waarde);
+            System.out.println(" ");
+            System.out.println(randomwaarde);
+        } else if (randomwaarde >= 9) {
+            System.out.println("Dit is uw kaart: " + kaartType.get(randomtype).type + " " + kaarten.get(randomwaarde).hogekaarten);
+            System.out.println(" ");
+            System.out.println(randomwaarde);
+        }
 
     }
 
     /*
         ToonKaartenSet Kan je de huidige kaarten deck zien of tonen.
     */
-    public void ToonKaartenSet() {
-        
-        for (Kaart nummer : MaakKaartenSet) {
-            for (Kaart soort : MaakKaartenSet) {
-
-                System.out.println("test of de kaart type 1 omhoog gaat. " + soort.type + nummer.waarde);
-                System.out.println("test of de kaart type 1 omhoog gaat. " + soort.type + nummer.hogekaarten);
-
-            }
-        }
-
+    static void ToonKaartenSet() {
         Random random = new Random();
         int random1 = random.nextInt(13);
         int random2 = random.nextInt(3);
-
-        if (random1 <= 8) {
-            System.out.println("Random1 lage kaarten == "+ kaarten.get(random1).waarde + " En random2 == " + kaartType.get(random2).type);
-        } else if (random1 >= 9) {
-            System.out.println("Random1 hogekaart == " + kaarten.get(random1).hogekaarten + " En random2 == " + kaartType.get(random2).type);
-        }
+        MaakKaartenSet(random2,random1,"");
     }
 
     public static void main(String[] args) {
-        Kaarten toonkaarten = new Kaarten();
-        toonkaarten.MaakKaartenSet();
-
-        /*
-        Hier wordt er een random nummer gegeven die vervolgens een kaart pakt.
-        Deze zal in de dealer/bank class worden gebruikt.
-         */
-
-
-        /*
-        Hier wordt er gekeken of de random lager of gelijk is aan index nummer 8 voor kaart nummer 10.
-        Als deze hoger is dan worden de hoge kaarten: Boer, Vrouw, Koning, Ä getrokken.
-        */
-
-/*
-        if (random1 <= 8) {
-            System.out.println("Random1 lage kaarten == "+ kaarten.get(random1).waarde +
-                                " En random2 == " + kaartType.get(random2).type);
-            System.out.println(" ");
-            System.out.println(random1);
-        } else if (random1 >= 9) {
-            System.out.println("Random1 hogekaart == " + kaarten.get(random1).hogekaarten +
-                                " En random2 == " + kaartType.get(random2).type);
-            System.out.println(" ");
-            System.out.println(random1);
-        }
-
- */
-    }
-}
-
-
-class Deck {
-    ArrayList<Kaart> kaarten;
-//    ArrayList<int> waarde;
-
-    Deck(ArrayList<Kaart> kaarten) {
-        this.kaarten = kaarten;
+        ToonKaartenSet();
     }
 }
 
